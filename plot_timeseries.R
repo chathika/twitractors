98a349f113c6be25e54a45cfd5230539c293c087
chathika <chathikagunaratne@gmail.com>
2018-07-02 16:17:31 -0400

    data fixes and R plotting code

--- plot_timeseries.R
+++ plot_timeseries.R
@@ -4,3 +4,5 @@ library(ggplot2)
 ggplot(sentiment_timeseries, aes(x= polarity, y = subjectivity )) + geom_point()
 ggplot(sentiment_timeseries, aes(x=as.numeric(rownames(sentiment_timeseries)), y = polarity) ) + geom_point()
 ggplot(sentiment_timeseries, aes(x=as.numeric(rownames(sentiment_timeseries)), y = subjectivity) ) + geom_point()
+
+write.csv(subset(sentiment_timeseries, select = c("polarity")), "polarity_timeseries_germex.csv", row.names = FALSE)
