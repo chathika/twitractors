1e98f6788bf175d8c8dea03d73bf30ac488d43cc
Ubuntu <ubuntu@ip-172-31-95-17.ec2.internal>
2018-12-02 07:47:28 +0000

    update to translation... uses AWS Translate instead of Google APIs"
    "

--- AnalyzeGerMex.py
+++ AnalyzeGerMex.py
@@ -8,26 +8,50 @@ import unicodedata
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
 from pandas.io.json import json_normalize
-from mtranslate import translate
+#from mtranslate import translate
 from multiprocessing import Pool
+import traceback
+import boto3
+from langdetect import detect
+import string
+import os.path
+import time
 
+def clean_tweet( tweet):
+    cleanString = re.sub('\n',' ', tweet )
+    cleanString = re.sub('\r',' ', cleanString )
+    cleanString = re.sub(',',' ', cleanString )
+    return cleanString
 def translateChunk(data):
     GerMexTweetsChunk = pd.DataFrame(data) 
     tweet_texts = GerMexTweetsChunk.text
+    #print(tweet_texts)
+    translate = boto3.client(service_name="translate", use_ssl=True)
     def tryTranslate(t):
+        text = t
         try:
-            return translate(str(t), "en", "auto") 
-        except:
-            return t
-    tweet_texts = tweet_texts.apply(lambda t: tryTranslate(t))
-    """for idx, tweet_text in tweet_texts.iteritems():
-        tweet_texts.set_value(idx, translate(str(tweet_text), "en", "auto"))"""
+            printable = set(string.printable)
+            text = clean_tweet(text)
+            text = "".join(filter(lambda x: x in printable, text))
+            language = detect(text)
+            translated = translate.translate_text(Text=text, SourceLanguageCode=language, TargetLanguageCode="en").get("TranslatedText") 
+            time.sleep(0.5)
+            return translated
+        except Exception:
+            traceback.print_exc()
+            return text
+    tweet_texts = tweet_texts.apply(lambda t:tryTranslate(t))
     GerMexTweetsChunk.text = tweet_texts
+    #print(GerMexTweetsChunk.text)
+    header_required = False
+    if os.path.isfile("chunk.csv"):
+        header_required = True
     with open('chunk.csv', 'a') as f:
-        GerMexTweetsChunk.to_csv(f)
+        GerMexTweetsChunk.to_csv(f, header=header_required, encoding='utf-8')
     print(GerMexTweetsChunk.text)
     #GerMexTweetsChunk.text = GerMexTweetsChunk.text.apply(lambda t: translate(str(t), "en", "auto"))
     #print(GerMexTweetsChunk.head())
+    #time.sleep(1)
     return GerMexTweetsChunk
 
 if __name__ == '__main__':
@@ -38,19 +62,20 @@ if __name__ == '__main__':
         print(file)
         returnval = []
         for line in open(file, mode="r"):
-            if "{" in line:
+            if "{\"created_at\"" in line:
                 returnval.append(json.loads(line))
             #yield json.loads(line['text'])
         return returnval
     tweets = json_readr(fileName)
     GerMexTweets=json_normalize(tweets)
     print("Main sending data to workers")
-    with Pool(processes=6) as pool:
-        result = pd.DataFrame(pool.map(translateChunk,np.array_split(GerMexTweets,6)))
-        print("Workers are done")
-        result = pd.DataFrame(result)        
-        result.to_csv("GerMexTweetsTranslated.csv")
-        print(result.head())
+    pool = Pool()
+    pool.map(translateChunk,np.array_split(GerMexTweets,1000))
+    #translateChunk(GerMexTweets)
+    print("Workers are done")
+    #result = pd.DataFrame(result)        
+    #result.to_csv("GerMexTweetsTranslated.csv")
+    #print(result.head())
     #!pip install langdetect
     #!pip install mtranslate
 
